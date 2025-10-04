b52528776b4c1cbddae433764fe8213d65472bb5
mineduck1608 <ducdm160804@gmail.com>
2025-10-04 11:20:50 +0700

    connect rabbitmq test

--- src/main/java/site/alphacode/alphacodepaymentservice/AlphaCodePaymentServiceApplication.java
+++ src/main/java/site/alphacode/alphacodepaymentservice/AlphaCodePaymentServiceApplication.java
@@ -1,5 +1,6 @@
 package site.alphacode.alphacodepaymentservice;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
@@ -12,6 +13,7 @@ import io.github.cdimascio.dotenv.Dotenv;
 @EnableScheduling
 @SpringBootApplication
 @EnableCaching
+@EnableRabbit
 public class AlphaCodePaymentServiceApplication {
 
     private final Environment environment;
--- src/main/java/site/alphacode/alphacodepaymentservice/producer/PaymentProducer.java
+++ src/main/java/site/alphacode/alphacodepaymentservice/producer/PaymentProducer.java
@@ -1,14 +1,21 @@
 package site.alphacode.alphacodepaymentservice.producer;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.stereotype.Service;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
-@Service
-public class PaymentProducer {
+@Component
+public class PaymentProducer implements CommandLineRunner {
 
     private final RabbitTemplate rabbitTemplate;
+
+    @Override
+    public void run(String... args) {
+        System.out.println("RabbitMQ connected: " + rabbitTemplate.getConnectionFactory().getHost());
+        rabbitTemplate.convertAndSend("payment.exchange", "course.create.queue", "Hello test message");
+    }
 
     public PaymentProducer(RabbitTemplate rabbitTemplate) {
         this.rabbitTemplate = rabbitTemplate;
